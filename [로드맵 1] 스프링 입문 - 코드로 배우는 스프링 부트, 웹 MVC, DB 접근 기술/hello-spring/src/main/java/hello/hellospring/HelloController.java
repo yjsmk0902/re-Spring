9979638afd7bc94cc7b34d8a578db792d5e458f6
Luke_YS <yjsmk0902@gmail.com>
2024-04-02 19:19:30 +0900

    :bookmark: 스프링 웹 개발 기초 - API(@ResponseBody 문자 반환) (#2)

--- [로드맵 1] 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/HelloController.java
+++ [로드맵 1] 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/HelloController.java
@@ -1,16 +1,16 @@
 package hello.hellospring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello-template";
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
