b99a8d9423c2af0fd70269aec05cd221b4ba8f95
Luke_YS <yjsmk0902@gmail.com>
2024-04-02 19:45:41 +0900

    :bookmark: 회원 관리 예제 - 백엔드 개발 - 회원 리포지토리 테스트 케이스 작성 (#2)

--- [로드맵 1] 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ [로드맵 1] 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import hello.hellospring.domain.Member;
 import java.util.*;
 
 //* 동시성 문제가 고려되어 있지 않음, 실무에서는 ConcurrentHashMap, AtomicLong 사용 고려
-public class MemberRepositoryImpl implements MemberRepository {
+public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
